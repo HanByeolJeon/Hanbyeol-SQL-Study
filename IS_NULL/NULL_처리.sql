e8c0f3ef1d638c5f35aa1343d5fce7f6eccefdfa
HanByeolJeon <caliconyang27@gmail.com>
2026-02-06 14:18:18 +0900

    Update NULL_처리.sql
    NULL 처리를 하려면 IFNULL(NAME, 'No name') AS NAME 식의 논리로 써주면 NULL의 유무에 상관없이 결과값을 나타낼 수 있다.

--- IS_NULL/NULL_처리.sql
+++ IS_NULL/NULL_처리.sql
@@ -1,4 +1,4 @@
--- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59410
+ -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59410
 SELECT
     ANIMAL_TYPE,
     IFNULL(NAME, 'No name') AS NAME,
