f62e7a1028f08bf735ad449e9d64894644e34eea
HanByeolJeon <caliconyang27@gmail.com>
2026-02-06 14:16:29 +0900

    Update 경기도에_위치한_식품창고_목록_출력.sql
    처음엔 냉동시절 여부가 NULL인 쿼리만 'N' 처리하라는 말에 NULL인 쿼리만 따로 세어서 'N'으로 표기하면 될 거라고 판단하고 SELECT count(FREEZER_YN) as 'N' 로 접근했지만 오류가 났는데 어디서 잘못된 논리인지 몰라서 AI 채팅한테 입력한 결과, COUNT는 행 개수를 세는 함수이므로 출력 요구랑 전혀 다른데다 'N'은 컬럼 별명이지, NULL을 대체하는 값이 아니기 때문에 그 논리가 틀린 것이다. 따라서 지역 필터링을 하려면 WHERE ADDRESS LIKE '경기도%' 식의 논리가 들어맞고, NULL 처리하려면 IFNULL(FREEZER_YN, 'N') 식의 논리가 들어 맞는다. 한 번에 두 가지의 논리를 알게 되었다.

--- IS_NULL/경기도에_위치한_식품창고_목록_출력.sql
+++ IS_NULL/경기도에_위치한_식품창고_목록_출력.sql
@@ -1,4 +1,4 @@
--- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131114
+ -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/131114
 SELECT
     WAREHOUSE_ID,
     WAREHOUSE_NAME,
