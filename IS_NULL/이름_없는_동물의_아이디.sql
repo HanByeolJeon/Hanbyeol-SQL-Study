49f640be8cf51016540572e518d51991b01710af
HanByeolJeon <caliconyang27@gmail.com>
2026-02-06 14:05:02 +0900

    Update 이름_없는_동물의_아이디.sql
    처음에는 select count (name) as null이랑 where animal_id 로 접근했지만 sql 쿼리에 문제가 생겼다. AI 채팅한테 물어본 결과로는 count는 null를 세지 않고, 행 자체를 세기 때문에 틀린 논리라는 걸 깨달았다. 따라서 select ANIMAL_ID랑 Where NAME is null로 접근해야 이름 없는 동물의 아이디만 나열할 수 있음을 배우게 되었다.

--- IS_NULL/이름_없는_동물의_아이디.sql
+++ IS_NULL/이름_없는_동물의_아이디.sql
@@ -1,4 +1,4 @@
--- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59039
+ -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59039
 SELECT ANIMAL_ID
 from ANIMAL_INS
 where NAME is null
