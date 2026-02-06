0a51c23a9ae250593886217fb32ba74b48e2682c
HanByeolJeon <caliconyang27@gmail.com>
2026-02-06 16:34:36 +0900

    Update 중복_제거.sql
    동물 보호소에 들어온 동물의 이름의 갯수를 세기 위해서는 SELECT COUNT(NAME) AS count 논리가 필요합니다. 
    
    그러나, 이름이 NULL인 동물은 집계하지 않고, 중복된 이름은 하나로 통일해야합니다. 따라서 SELECT COUNT(DISTINCT NAME) AS count 논리랑 WHERE NAME IS NOT NULL 논리가 필요합니다.

--- Programmers/SUM_MAX_MIN/중복_제거.sql
+++ Programmers/SUM_MAX_MIN/중복_제거.sql
@@ -1,4 +1,4 @@
--- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59408
+ -- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59408
 SELECT COUNT(DISTINCT NAME) AS count
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL;
